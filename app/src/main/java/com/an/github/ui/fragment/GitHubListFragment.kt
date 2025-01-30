485498e5f87baf306490bcd49aad746f4ee86d56
Anitaa Murthy <murthyanitaa@gmail.com>
2025-01-30 14:51:54 +0530

    Added GitHubListFragment UI

--- app/src/main/java/com/an/github/ui/fragment/GitHubListFragment.kt
+++ app/src/main/java/com/an/github/ui/fragment/GitHubListFragment.kt
@@ -1,6 +1,21 @@
 package com.an.github.ui.fragment
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.an.github.databinding.FragmentGithubListBinding
 
 class GitHubListFragment: Fragment() {
+
+    private lateinit var binding: FragmentGithubListBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentGithubListBinding.inflate(inflater, container, false)
+        return binding.root
+    }
 }
